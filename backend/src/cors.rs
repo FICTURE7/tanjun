f07931eec5e5e4ee2c97f8e6abf55a6172350946
FICTURE7 <ficture7@gmail.com>
2025-03-22 23:10:40 +0400

    backend: Add support for pre-flight OPTIONS request

--- backend/src/cors.rs
+++ backend/src/cors.rs
@@ -1,7 +1,8 @@
+use std::io::Cursor;
 use rocket::Request;
 use rocket::response::Response;
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
+use rocket::http::{Status, Method, ContentType, Header};
 
 pub struct Cors;
 
@@ -14,10 +15,16 @@ impl Fairing for Cors {
     }
   }
   
-  async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+  async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
     response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
     response.set_header(Header::new("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET"));
     response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
     response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+    if request.method() == Method::Options {
+      response.set_status(Status::Ok);
+      response.set_header(ContentType::Plain);
+      response.set_sized_body(Some(0), Cursor::new(""));
+    }
   }
 }
