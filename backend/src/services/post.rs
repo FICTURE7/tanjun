15acd7d8a89915c6f58be1da5a4a154bf9f004ce
FICTURE7 <ficture7@gmail.com>
2025-03-24 21:24:12 +0400

    backend: Pass `SqliteRow` by reference to reduce code duplication

--- backend/src/services/post.rs
+++ backend/src/services/post.rs
@@ -1,4 +1,5 @@
 use sqlx::{Row, SqliteConnection};
+use sqlx::sqlite::SqliteRow;
 
 use crate::models::user::User;
 use crate::models::post::{Post, NewPost, UpdatePost};
@@ -26,7 +27,7 @@ pub async fn create(conn: &mut SqliteConnection, author_id: i64, post: &NewPost)
     .bind(author_id)
     .fetch_one(conn)
     .await
-    .map(map_row)
+    .map(|row| map_row(&row))
     .map_err(|e| Error::Database(e.to_string()))
 }
 
@@ -47,7 +48,7 @@ pub async fn read(conn: &mut SqliteConnection, id: i64) -> Result<Option<Post>, 
     .bind(&id)
     .fetch_optional(conn)
     .await
-    .map(|row| row.map(map_row))
+    .map(|row| row.map(|row| map_row(&row)))
     .map_err(|e| Error::Database(e.to_string()))
 }
 
@@ -67,17 +68,7 @@ pub async fn read_paged(conn: &mut SqliteConnection) -> Result<Vec<Post>, Error>
     "#)
     .fetch_all(conn)
     .await
-    .map(|rows| rows.iter().map(|row| Post {
-      id: row.get(0),
-      title: row.get(1),
-      content: row.get(2),
-      created_at: row.get(3),
-      updated_at: row.get(4),
-      author: User {
-        id: row.get(5),
-        username: row.get(6),
-      }
-    }).collect())
+    .map(|rows| rows.iter().map(|row| map_row(row)).collect())
     .map_err(|e| Error::Database(e.to_string()))
 }
 
@@ -105,7 +96,7 @@ pub async fn update(conn: &mut SqliteConnection, id: i64, post: &UpdatePost) -> 
     .bind(&id)
     .fetch_optional(conn)
     .await
-    .map(|row| row.map(map_row))
+    .map(|row| row.map(|row| map_row(&row)))
     .map_err(|e| Error::Database(e.to_string()))
 }
 
@@ -129,11 +120,11 @@ pub async fn delete(conn: &mut SqliteConnection, id: i64) -> Result<Option<Post>
     .bind(&id)
     .fetch_optional(conn)
     .await
-    .map(|row| row.map(map_row))
+    .map(|row| row.map(|row| map_row(&row)))
     .map_err(|e| Error::Database(e.to_string()))
 }
 
-fn map_row(row: sqlx::sqlite::SqliteRow) -> Post {
+fn map_row(row: &SqliteRow) -> Post {
   Post {
     id: row.get(0),
     title: row.get(1),
